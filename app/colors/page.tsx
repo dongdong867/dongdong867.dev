416b07b93d2128b851aa41ffca0ba435e739b124
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-07-13 22:03:28 +0800

    feat(colors): add colors page for theme editing

---
+++ app/colors/page.tsx
@@ -0,0 +1,10 @@
+const ColorsPage = () => {
+  return (
+    <>
+      <div className="w-screen h-20 bg-primary">primary</div>
+      <div className="w-screen h-20 bg-secondary">secondary</div>
+    </>
+  );
+};
+
+export default ColorsPage;
