022fdd3c0149015d6cbf18d4235c8cbd166e12d6
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-07-20 20:06:33 +0800

    feat(colors): remove dark div, add theme provider

--- app/colors/page.tsx
+++ app/colors/page.tsx
@@ -1,6 +1,9 @@
+import ThemeProvider from "@/components/theme-provider";
+
 const ColorsPage = () => {
   return (
     <>
+      <ThemeProvider />
       <div className="w-screen h-20 bg-primary">primary</div>
       <div className="w-screen h-20 bg-primary-foreground">
         primary-foreground
@@ -30,40 +33,6 @@ const ColorsPage = () => {
       <div className="w-screen h-20 bg-card-foreground">card-foreground</div>
       <div className="w-screen h-20 bg-border">border</div>
       <div className="w-screen h-20 bg-input">input</div>
-      <div className="dark">
-        <h1>dark</h1>
-        <div className="w-screen h-20 bg-primary">primary</div>
-        <div className="w-screen h-20 bg-primary-foreground">
-          primary-foreground
-        </div>
-        <div className="w-screen h-20 bg-secondary">secondary</div>
-        <div className="w-screen h-20 bg-secondary-foreground">
-          secondary-foreground
-        </div>
-        <div className="w-screen h-20 bg-accent">accent</div>
-        <div className="w-screen h-20 bg-accent-foreground">
-          accent-foreground
-        </div>
-        <div className="w-screen h-20 bg-destructive">destructive</div>
-        <div className="w-screen h-20 bg-destructive-foreground">
-          destructive-foreground
-        </div>
-        <div className="w-screen h-20 bg-ring">ring</div>
-        <div className="w-screen h-20 bg-background">background</div>
-        <div className="w-screen h-20 bg-foreground">foreground</div>
-        <div className="w-screen h-20 bg-muted">muted</div>
-        <div className="w-screen h-20 bg-muted-foreground">
-          muted-foreground
-        </div>
-        <div className="w-screen h-20 bg-popover">popover</div>
-        <div className="w-screen h-20 bg-popover-foreground">
-          popover-foreground
-        </div>
-        <div className="w-screen h-20 bg-card">card</div>
-        <div className="w-screen h-20 bg-card-foreground">card-foreground</div>
-        <div className="w-screen h-20 bg-border">border</div>
-        <div className="w-screen h-20 bg-input">input</div>
-      </div>
     </>
   );
 };
