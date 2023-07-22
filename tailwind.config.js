b86708e67361f658c982a343378571613602cf21
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-07-23 04:31:54 +0800

    feat(tailwind): update colors settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,19 @@ module.exports = {
     },
     extend: {
       colors: {
+        red: "var(--red)",
+        orange: "var(--orange)",
+        yellow: "var(--yellow)",
+        green: "var(--green)",
+        mint: "var(--mint)",
+        teal: "var(--teal)",
+        cyan: "var(--cyan)",
+        blue: "var(--blue)",
+        indigo: "var(--indigo)",
+        purple: "var(--purple)",
+        pink: "var(--pink)",
+        brown: "var(--brown)",
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -25,31 +38,11 @@ module.exports = {
         foreground: "var(--foreground)",
         primary: {
           DEFAULT: "var(--primary)",
-          foreground: "hsl(var(--primary-foreground))",
+          foreground: "var(--primary-foreground)",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
         },
       },
       borderRadius: {
