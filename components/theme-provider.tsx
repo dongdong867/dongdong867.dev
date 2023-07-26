eab4b07de28e93ec50949ca02e682830f27865d5
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-07-27 01:00:47 +0800

    refactor(theme-provider): update button usage

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { LuSun, LuMoon, LuMonitor } from "react-icons/lu";
-import { Button } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,26 +8,10 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { useEffect, useState } from "react";
+import TranslatableContextButton from "./translatable-context-button";
 
 const ThemeProvider = () => {
   const [mode, setMode] = useState("system");
-
-  const [top, setTop] = useState(0);
-  const [left, setLeft] = useState(0);
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
-
-  const [clientX, setClientX] = useState(0);
-  const [clientY, setClientY] = useState(0);
-
-  const handleRef = (element: HTMLButtonElement) => {
-    if (element) {
-      setTop(element.offsetTop);
-      setLeft(element.offsetLeft);
-      setWidth(element.offsetWidth);
-      setHeight(element.offsetHeight);
-    }
-  };
 
   useEffect(() => {
     if (
@@ -43,59 +26,16 @@ const ThemeProvider = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          ref={handleRef}
-          className="w-10 h-10 p-0 bg-transparent text-2xl hover:bg-gray/50 hover:cursor-none focus-visible:ring-transparent focus-visible:ring-offset-0"
-          onMouseMove={(e) => {
-            if (e.clientX < left + width / 2 && e.clientY < top + height / 2) {
-              setClientX(((e.clientX - left - width) / width) * 20);
-              setClientY(((e.clientY - top - height) / height) * 20);
-              return;
-            }
-            if (e.clientX < left + width && e.clientY < top + height / 2) {
-              setClientX(((e.clientX - left - width / 2) / width) * 40);
-              setClientY(((e.clientY - top - height) / height) * 20);
-              return;
-            }
-            if (e.clientX < left + width / 2 && e.clientY < top + height) {
-              setClientX(((e.clientX - left - width) / width) * 20);
-              setClientY(((e.clientY - top - height / 2) / height) * 40);
-              return;
-            }
-            if (e.clientX < left + width && e.clientY < top + height) {
-              setClientX(((e.clientX - left - width / 2) / width) * 40);
-              setClientY(((e.clientY - top - height / 2) / height) * 40);
-            }
-          }}
-          onMouseLeave={() => {
-            setClientX(0);
-            setClientY(0);
-          }}
-        >
+        <TranslatableContextButton className="w-10 h-10 p-0 bg-transparent text-2xl hover:bg-gray/50 hover:cursor-none focus-visible:ring-transparent focus-visible:ring-offset-0">
           {mode !== "system" ? (
             <>
-              <LuSun
-                className="dark:hidden"
-                style={{
-                  transform: `translate(${clientX}%, ${clientY}%)`,
-                }}
-              />
-              <LuMoon
-                className="hidden dark:inline"
-                style={{
-                  transform: `translate(${clientX}%, ${clientY}%)`,
-                }}
-              />
+              <LuSun className="dark:hidden" />
+              <LuMoon className="hidden dark:inline" />
             </>
           ) : (
-            <LuMonitor
-              style={{
-                transform: `translate(${clientX}%, ${clientY}%)`,
-                top: `20px`,
-              }}
-            />
+            <LuMonitor />
           )}
-        </Button>
+        </TranslatableContextButton>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="bg-background">
         <DropdownMenuItem
