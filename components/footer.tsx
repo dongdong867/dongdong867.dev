0735110cc3b99e8328d6b4042ef4a3bf112d66c2
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-07-31 03:00:03 +0800

    feat(footer): add footer context and icons

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,43 @@
+import {
+  FaGithub,
+  FaLinkedin,
+  FaInstagram,
+  FaDiscord,
+  FaEnvelope,
+} from "react-icons/fa";
+import TranslatableContextButton from "./translatable-context-button";
+
 const Footer = () => {
   return (
-    <div>
-      <div></div>
+    <div className="flex justify-between place-items-center pb-10 mx-10">
+      <div>Copyright©2023 Dongdong867</div>
+      <div className="flex">
+        <TranslatableContextButton>
+          <div className="text-xl">
+            <FaGithub />
+          </div>
+        </TranslatableContextButton>
+        <TranslatableContextButton>
+          <div className="text-xl">
+            <FaLinkedin />
+          </div>
+        </TranslatableContextButton>
+        <TranslatableContextButton>
+          <div className="text-xl">
+            <FaInstagram />
+          </div>
+        </TranslatableContextButton>
+        <TranslatableContextButton>
+          <div className="text-xl">
+            <FaDiscord />
+          </div>
+        </TranslatableContextButton>
+        <TranslatableContextButton>
+          <div className="text-xl">
+            <FaEnvelope />
+          </div>
+        </TranslatableContextButton>
+      </div>
     </div>
   );
 };
