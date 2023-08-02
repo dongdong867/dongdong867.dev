cae9a122a2b34a8afc299d6b3358d5ed92a474cb
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-03 02:28:18 +0800

    feat(home): create homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-import Image from "next/image";
+import TranslatableContextButton from "@/components/translatable-context-button";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
-    <div className="cursor-[url(/cursor.png),_default] w-screen h-[calc(100vh-7.25rem)]"></div>
+    <div className="w-screen h-[calc(100vh-7.25rem)] text-center flex justify-center place-items-center">
+      <div>
+        <div className="font-display font-black -space-y-4">
+          <div className="text-3xl">Hi, I'm</div>
+          <div className="h-full text-7xl leading-normal text-transparent bg-title-gradient bg-clip-text">
+            Dongdong867.
+          </div>
+        </div>
+        <div className="flex justify-center place-items-center space-x-1 mt-2">
+          <TranslatableContextButton className="text-lg">
+            A Student
+          </TranslatableContextButton>
+          <Separator
+            orientation="vertical"
+            className="w-0.5 h-6 bg-foreground"
+          />
+          <TranslatableContextButton className="text-lg">
+            A Full Stack Developer
+          </TranslatableContextButton>
+          <Separator
+            orientation="vertical"
+            className="w-0.5 h-6 bg-foreground"
+          />
+          <TranslatableContextButton className="text-lg">
+            A UI/ UX Designer
+          </TranslatableContextButton>
+        </div>
+      </div>
+    </div>
   );
 }
