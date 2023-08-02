7a08fd499a6056cfea1cf7d4936926b312a60327
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-03 05:22:30 +0800

    feat(footer): add cursor settings

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,17 +14,29 @@ const Footer = () => {
       <div>Copyright © 2023 Dongdong867</div>
       <div className="flex">
         <TranslatableContext className="p-2">
-          <Link href={"https://github.com//dongdong867"} target="_blank">
+          <Link
+            href={"https://github.com//dongdong867"}
+            target="_blank"
+            className="cursor-none"
+          >
             <FaGithub className="text-2xl" />
           </Link>
         </TranslatableContext>
         <TranslatableContext className="p-2">
-          <Link href={"https://linkedin.com/in/dongdong867"} target="_blank">
+          <Link
+            href={"https://linkedin.com/in/dongdong867"}
+            target="_blank"
+            className="cursor-none"
+          >
             <FaLinkedin className="text-2xl" />
           </Link>
         </TranslatableContext>
         <TranslatableContext className="p-2">
-          <Link href={"https://instagram.com/dongdong_867"} target="_blank">
+          <Link
+            href={"https://instagram.com/dongdong_867"}
+            target="_blank"
+            className="cursor-none"
+          >
             <FaInstagram className="text-2xl" />
           </Link>
         </TranslatableContext>
@@ -32,12 +44,13 @@ const Footer = () => {
           <Link
             href={"https://discordapp.com/users/978679087067435039"}
             target="_blank"
+            className="cursor-none"
           >
             <FaDiscord className="text-2xl" />
           </Link>
         </TranslatableContext>
         <TranslatableContext className="p-2">
-          <Link href={"mailto:dongdong0867@gmail.com"}>
+          <Link href={"mailto:dongdong0867@gmail.com"} className="cursor-none">
             <FaEnvelope className="text-2xl" />
           </Link>
         </TranslatableContext>
