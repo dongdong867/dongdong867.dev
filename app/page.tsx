1725cf839873aa60e719c8b4b293009bb1d672be
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-04 02:22:08 +0800

    feat(home): add responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,70 @@
 import TranslatableContext from "@/components/translatable-context";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
+import { FiArrowUpRight } from "react-icons/fi";
 
 export default function Home() {
   return (
     <div className="w-screen h-[calc(100vh-7.25rem)] text-center flex justify-center place-items-center">
       <div>
-        <div className="font-display font-black -space-y-4  cursor-default">
-          <div className="text-3xl">Hi, I'm</div>
-          <div className="h-full text-6xl leading-normal text-transparent bg-title-gradient bg-clip-text">
+        <div className="font-display font-black -space-y-2 cursor-default md:-space-y-4">
+          <div className="text-xl md:text-3xl">Hi, I'm</div>
+          <div
+            className="
+              h-full 
+              text-4xl text-transparent 
+              leading-normal 
+              bg-title-gradient bg-clip-text 
+              md:text-6xl md:leading-normal"
+          >
             Dongdong867.
           </div>
         </div>
-        <div className="h-max flex justify-center place-items-center space-x-1 font-semibold">
+        <div
+          className="
+            w-full h-max 
+            m-auto 
+            font-semibold 
+            flex justify-center place-items-center 
+            max-sm:flex-col max-sm:place-items-start 
+            max-sm:mt-2
+            md:space-x-1"
+        >
           <TranslatableContext className="text-base px-3 py-1">
             <Link href={"/student"} className="cursor-none">
-              A Student
+              <div className="w-full flex justify-between place-items-center space-x-1 sm:hidden">
+                <div>A Student</div>
+                <FiArrowUpRight />
+              </div>
+              <div className="max-sm:hidden">A Student</div>
             </Link>
           </TranslatableContext>
           <Separator
             orientation="vertical"
-            className="w-0.5 h-5 bg-foreground"
+            className="w-0.5 h-5 bg-foreground max-sm:hidden"
           />
+          <Separator className="w-full h-0.5 bg-foreground sm:hidden" />
           <TranslatableContext className="text-base px-3 py-1">
             <Link href={"/fullstack"} className="cursor-none">
-              A Full Stack Developer
+              <div className="w-full flex justify-between place-items-center space-x-1 sm:hidden">
+                <div>A Full Stack Developer</div>
+                <FiArrowUpRight />
+              </div>
+              <div className="max-sm:hidden">A Full Stack Developer</div>
             </Link>
           </TranslatableContext>
           <Separator
             orientation="vertical"
-            className="w-0.5 h-5 bg-foreground"
+            className="w-0.5 h-5 bg-foreground max-sm:hidden"
           />
+          <Separator className="w-full h-0.5 bg-foreground sm:hidden" />
           <TranslatableContext className="text-base px-3 py-1">
             <Link href={"/uiux"} className="cursor-none">
-              A UI/ UX Designer
+              <div className="w-full flex justify-between place-items-center space-x-1 sm:hidden">
+                <div>A UI/ UX Designer</div>
+                <FiArrowUpRight />
+              </div>
+              <div className="max-sm:hidden">A UI/ UX Designer</div>
             </Link>
           </TranslatableContext>
         </div>
