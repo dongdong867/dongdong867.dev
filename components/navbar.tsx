49aa59417dbb6f2c3a7c31119d3e13eecc220303
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-04 04:03:51 +0800

    fix(navbar): update nav link to component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,10 @@
 import ThemeProvider from "./theme-provider";
 import Image from "next/image";
 import dong_logo from "@/public/dong-logo.svg";
-import TranslatableContext from "./translatable-context";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "./ui/alert-dialog";
+import NavigationMenu from "./navigation-menu";
 
 const Navbar = () => (
-  <div className="flex justify-between place-items-center mx-10 mt-4">
+  <div className="flex justify-between place-items-center mx-10 mt-4 max-sm:mx-6 max-sm:mt-6">
     <Image
       src={dong_logo}
       alt="dong-logo"
@@ -21,50 +12,9 @@ const Navbar = () => (
       height={2000}
       className="w-max h-5 ring-0 dark:invert"
     />
-    <div className="flex place-items-center space-x-4">
+    <div className="flex place-items-center">
       <ThemeProvider />
-      {/* <TranslatableContext className="text-xl p-3"> */}
-      {/* <Link href={"/about"}>About</Link> */}
-      <AlertDialog>
-        <AlertDialogTrigger>About</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>This page is still building.</AlertDialogTitle>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogAction>Close</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-      {/* </TranslatableContext> */}
-      {/* <TranslatableContext className="text-xl p-3"> */}
-      {/* <Link href={"/works"}>Works</Link> */}
-      <AlertDialog>
-        <AlertDialogTrigger>Works</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>This page is still building.</AlertDialogTitle>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogAction>Close</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-      {/* </TranslatableContext> */}
-      {/* <TranslatableContext className="text-xl p-3"> */}
-      {/* <Link href={"/connect"}>Connect</Link> */}
-      <AlertDialog>
-        <AlertDialogTrigger>Connect</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>This page is still building.</AlertDialogTitle>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogAction>Close</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-      {/* </TranslatableContext> */}
+      <NavigationMenu />
     </div>
   </div>
 );
