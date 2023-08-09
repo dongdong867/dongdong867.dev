362004148d7b5c44c7d8183095767f6c849879c2
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-10 05:50:20 +0800

    fix(uiux): update rounded value

--- app/uiux/page.tsx
+++ app/uiux/page.tsx
@@ -5,10 +5,21 @@ const UIUXPage = () => {
   return (
     <div>
       <div className="w-full h-full">
-        <div className="w-max h-max m-auto my-10 rounded-[3rem] overflow-hidden">
+        <div className="w-max h-max m-auto my-10 rounded-[4rem] overflow-hidden">
           <video src="/uiux/logo-animation.mp4" autoPlay muted loop />
         </div>
         <NavTitle />
+      </div>
+      <div>
+        <div className="w-1/4 p-20 rounded-[3rem] bg-gradient-to-r from-[#48607b] to-[#a2adbf]">
+          <Image
+            src={"/dong-icon.png"}
+            alt="dong icon"
+            width={2000}
+            height={2000}
+            className="w-full"
+          />
+        </div>
       </div>
     </div>
   );
