7a5aa71e724f41cfd906a33c6feeadf751ac89aa
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-10 04:39:07 +0800

    feat(uiux): add animation and nav title

--- app/uiux/page.tsx
+++ app/uiux/page.tsx
@@ -1,7 +1,15 @@
+import NavTitle from "@/components/nav-title";
+import Image from "next/image";
+
 const UIUXPage = () => {
   return (
     <div>
-      <div className="w-full h-full"></div>
+      <div className="w-full h-full">
+        <div className="w-max h-max m-auto my-10 rounded-[3rem] overflow-hidden">
+          <video src="/logo-animation.mp4" autoPlay muted loop />
+        </div>
+        <NavTitle />
+      </div>
     </div>
   );
 };
