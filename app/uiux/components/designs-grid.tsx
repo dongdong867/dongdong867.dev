c12b4f16525c127c98c44804bc62215b569767e3
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-12 02:58:09 +0800

    feat(designs-grid): add dong icon to page

--- app/uiux/components/designs-grid.tsx
+++ app/uiux/components/designs-grid.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 const DesignsGrid = () => {
   return (
-    <div>
-      <div></div>
+    <div className="grid grid-flow-row grid-cols-12 row-auto gap-6">
+      <div className="col-span-5 row-span-2 aspect-square flex justify-center place-items-center rounded-[3rem] bg-gradient-to-r from-[#48607b] to-[#a2adbf]">
+        <Image
+          src={"/dong-icon.png"}
+          alt="dong icon"
+          width={2000}
+          height={2000}
+          className="w-2/3 h-auto aspect-auto"
+        />
+      </div>
     </div>
   );
 };
