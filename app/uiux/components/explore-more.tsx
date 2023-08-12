2088a2119876dbe70da8f6dcaa2b74116c83e8ac
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-13 05:57:05 +0800

    feat(explore-more): create component, add context

---
+++ app/uiux/components/explore-more.tsx
@@ -0,0 +1,17 @@
+import TranslatableContext from "@/components/translatable-context";
+
+const ExploreMore = () => {
+  return (
+    <div className="w-max font-display font-black translate-x-[20%] my-60">
+      <div className="text-6xl">
+        <div>Explore</div>
+        <div>More in</div>
+      </div>
+      <TranslatableContext className="p-2 rounded-xl">
+        <div className="text-8xl">WORKS.</div>
+      </TranslatableContext>
+    </div>
+  );
+};
+
+export default ExploreMore;
