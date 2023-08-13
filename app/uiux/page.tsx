293420c60ade4924ae16e175eb604177bfc0013a
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-14 05:17:36 +0800

    feat(uiux): add rwd rounded and fix margin

--- app/uiux/page.tsx
+++ app/uiux/page.tsx
@@ -6,7 +6,7 @@ const UIUXPage = () => {
   return (
     <div>
       <div className="w-full h-full">
-        <div className="w-4/5 h-max m-auto my-10 rounded-[4rem] overflow-hidden">
+        <div className="w-4/5 h-max m-auto my-10 rounded-[4rem] overflow-hidden max-sm:rounded-3xl">
           <video
             src="/uiux/logo-animation.mp4"
             autoPlay
@@ -17,7 +17,7 @@ const UIUXPage = () => {
         </div>
         <NavTitle />
       </div>
-      <div className="m-8">
+      <div className="my-8">
         <DesignsGrid />
       </div>
       <div>
