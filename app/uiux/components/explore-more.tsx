7b5ec5ca269e463d17bd02a127f2668730187987
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-08-16 00:54:43 +0800

    feat(explore-more): add rwd design to component

--- app/uiux/components/explore-more.tsx
+++ app/uiux/components/explore-more.tsx
@@ -2,13 +2,15 @@ import TranslatableContext from "@/components/translatable-context";
 
 const ExploreMore = () => {
   return (
-    <div className="w-max font-display font-black translate-x-[20%] my-60">
-      <div className="text-6xl">
+    <div className="w-2/3 m-auto font-display font-black my-60 max-sm:my-40">
+      <div className="text-6xl max-sm:text-3xl">
         <div>Explore</div>
         <div>More in</div>
       </div>
       <TranslatableContext className="p-2 rounded-xl">
-        <div className="text-8xl underline underline-offset-8">WORKS.</div>
+        <div className="text-8xl underline underline-offset-8 max-sm:text-5xl">
+          WORKS.
+        </div>
       </TranslatableContext>
     </div>
   );
