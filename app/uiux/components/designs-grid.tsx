eec8c6c1ebd912eef31ebdd93f5bda2d50a4502f
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-09-12 20:31:09 +0800

    style(designs-grid): format class in file

--- app/uiux/components/designs-grid.tsx
+++ app/uiux/components/designs-grid.tsx
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 const DesignsGrid = () => {
   return (
-    <div className="w-2/3 grid grid-flow-row-dense grid-cols-12 row-auto gap-6 m-auto max-sm:grid-cols-8 max-sm:w-3/4 max-sm:gap-4">
-      <div className="col-span-5 row-span-2 aspect-square flex justify-center place-items-center rounded-[3rem] bg-gradient-to-r from-[#48607b] to-[#a2adbf] max-md:rounded-3xl">
+    <div
+      className="
+        w-2/3 
+        grid grid-flow-row-dense 
+        grid-cols-12 row-auto 
+        gap-6 m-auto 
+        max-sm:grid-cols-8 m
+        ax-sm:w-3/4 
+        max-sm:gap-4"
+    >
+      <div
+        className="
+          col-span-5 row-span-2 aspect-square 
+          flex justify-center place-items-center 
+          rounded-[3rem] max-md:rounded-3xl 
+          bg-gradient-to-r from-[#48607b] to-[#a2adbf] "
+      >
         <Image
           src={"/dong-icon.png"}
           alt="dong icon"
@@ -12,8 +27,21 @@ const DesignsGrid = () => {
           className="w-2/3 h-auto aspect-auto"
         />
       </div>
-      <div className="col-span-3 row-span-2 flex flex-col justify-between place-items-center space-y-6 max-sm:space-y-4">
-        <div className="w-full h-1/2 flex justify-center place-items-center rounded-[2rem] bg-neutral/40 max-md:rounded-2xl">
+      <div
+        className="
+          col-span-3 row-span-2 
+          flex flex-col 
+          justify-between 
+          place-items-center s
+          pace-y-6 max-sm:space-y-4"
+      >
+        <div
+          className="
+            w-full h-1/2 
+            flex justify-center place-items-center 
+            rounded-[2rem] max-md:rounded-2xl
+            bg-neutral/40"
+        >
           <Image
             src={"/uiux/lets-beat-google.svg"}
             alt="let's beat google"
@@ -22,7 +50,13 @@ const DesignsGrid = () => {
             className="w-5/12 h-auto aspect-auto"
           />
         </div>
-        <div className="w-full h-1/2 flex justify-center place-items-center rounded-[2rem] bg-neutral/40 max-md:rounded-2xl">
+        <div
+          className="
+            w-full h-1/2 
+            flex justify-center place-items-center 
+            rounded-[2rem] max-md:rounded-2xl
+            bg-neutral/40"
+        >
           <Image
             src={"/uiux/github-issue-manager.svg"}
             alt="github issue manager"
@@ -32,7 +66,15 @@ const DesignsGrid = () => {
           />
         </div>
       </div>
-      <div className="w-full h-full aspect-[3/1] col-span-8 row-span-1 flex justify-center place-items-center rounded-[2rem] bg-neutral/40 max-sm:rounded-3xl">
+      <div
+        className="
+          w-full h-full 
+          aspect-[3/1] 
+          col-span-8 row-span-1 
+          flex justify-center place-items-center 
+          rounded-[2rem] max-sm:rounded-3xl
+          bg-neutral/40"
+      >
         <Image
           src={"/uiux/tricargo-logo.svg"}
           alt="tricargo"
@@ -41,7 +83,16 @@ const DesignsGrid = () => {
           className="w-4/5 h-auto aspect-auto"
         />
       </div>
-      <div className="h-full aspect-[1/2] col-span-4 row-span-3 flex flex-col justify-center place-items-center rounded-[2rem] bg-neutral/40 max-sm:rounded-3xl">
+      <div
+        className="
+          h-full 
+          aspect-[1/2] 
+          col-span-4 row-span-3 
+          flex flex-col 
+          justify-center place-items-center 
+          rounded-[2rem] max-sm:rounded-3xl
+          bg-neutral/40"
+      >
         <Image
           src={"/uiux/tricargo.png"}
           alt="tricargo"
@@ -51,7 +102,16 @@ const DesignsGrid = () => {
         />
       </div>
 
-      <div className="h-full aspect-[1/2] col-span-4 row-span-3 flex flex-col justify-center place-items-center rounded-[2rem] bg-neutral/40 max-sm:rounded-3xl">
+      <div
+        className="
+          h-full 
+          aspect-[1/2] 
+          col-span-4 row-span-3 
+          flex flex-col 
+          justify-center place-items-center 
+          rounded-[2rem] max-sm:rounded-3xl
+          bg-neutral/40"
+      >
         <Image
           src={"/uiux/go-cafe.png"}
           alt="go cafe"
@@ -60,7 +120,15 @@ const DesignsGrid = () => {
           className="w-4/5 h-auto aspect-auto"
         />
       </div>
-      <div className="w-full h-full aspect-[3/1] col-span-8 row-span-1 flex justify-center place-items-center rounded-[2rem] bg-neutral/40 max-sm:rounded-3xl">
+      <div
+        className="
+          w-full h-full 
+          aspect-[3/1] 
+          col-span-8 row-span-1 
+          flex justify-center place-items-center 
+          rounded-[2rem] max-sm:rounded-3xl
+          bg-neutral/40"
+      >
         <Image
           src={"/dong-logo.svg"}
           alt="dong logo"
@@ -69,8 +137,20 @@ const DesignsGrid = () => {
           className="w-4/5 h-auto aspect-auto dark:invert"
         />
       </div>
-      <div className="col-span-3 row-span-2 flex flex-col justify-between place-items-center space-y-6">
-        <div className="w-full h-1/2 flex justify-center place-items-center rounded-[2rem] bg-neutral/40 max-sm:rounded-3xl">
+      <div
+        className="
+          col-span-3 row-span-2 
+          flex flex-col 
+          justify-between place-items-center 
+          space-y-6"
+      >
+        <div
+          className="
+            w-full h-1/2 
+            flex justify-center place-items-center 
+            rounded-[2rem] max-sm:rounded-3xl
+            bg-neutral/40"
+        >
           <Image
             src={"/uiux/nccu-job-bank.svg"}
             alt="NCCU Job Bank"
@@ -79,7 +159,13 @@ const DesignsGrid = () => {
             className="w-1/2 h-auto aspect-auto"
           />
         </div>
-        <div className="w-full h-1/2 flex justify-center place-items-center rounded-[2rem] bg-foreground max-sm:rounded-3xl">
+        <div
+          className="
+            w-full h-1/2 
+            flex justify-center place-items-center 
+            rounded-[2rem] max-sm:rounded-3xl
+            bg-foreground"
+        >
           <Image
             src={"/uiux/gocafe.png"}
             alt="github issue manager"
@@ -89,7 +175,14 @@ const DesignsGrid = () => {
           />
         </div>
       </div>
-      <div className="col-span-5 row-span-2 aspect-square flex justify-center place-items-center rounded-[3rem] bg-coming-soon-icon-gradient max-sm:rounded-3xl">
+      <div
+        className="
+          col-span-5 row-span-2 
+          aspect-square 
+          flex justify-center place-items-center 
+          rounded-[3rem] max-sm:rounded-3xl
+          bg-coming-soon-icon-gradient"
+      >
         <Image
           src={"/uiux/coming-soon.svg"}
           alt="coming soon"
