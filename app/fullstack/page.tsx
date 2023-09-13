d8fe44ae12721532962d3bf55d913d71f0be62a2
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-09-13 23:39:27 +0800

    feat(fullstack): add margin-y to nac-title

--- app/fullstack/page.tsx
+++ app/fullstack/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import NavTitle from "@/components/nav-title";
 
 const FullStackPage = () => {
   return (
     <div>
-      <NavTitle />
+      <div className="my-20">
+        <NavTitle />
+      </div>
     </div>
   );
 };
