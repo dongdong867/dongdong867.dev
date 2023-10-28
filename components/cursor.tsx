6d9575a8a2180c1e5f161c053d5c46fd3f4d0c62
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-10-28 11:34:39 +0800

    style(cursor): format code

--- components/cursor.tsx
+++ components/cursor.tsx
@@ -15,7 +15,15 @@ const Cursor = () => {
         left: `${state.docX}px`,
         top: `${state.docY}px`,
       }}
-      className={`absolute w-10 h-10 -translate-x-5 -translate-y-5 bg-gray-400/30 rounded-full pointer-events-none z-50`}
+      className={`
+        absolute w-10 h-10
+        -translate-x-5 -translate-y-5
+        bg-gray-400/30
+        rounded-full
+        pointer-events-none
+        cursor
+        z-50
+      `}
     />
   );
 };
