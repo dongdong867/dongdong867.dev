716586e7795fb0d34d240c6b4a8e04952de0573d
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-10-29 13:24:03 +0800

    fix(index): fix content height

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import NavTitle from "@/components/nav-title";
 
 export default function Home() {
   return (
-    <div className="w-screen h-[calc(100vh-7.5rem)] text-center flex justify-center place-items-center max-sm:h-[calc(100vh-8.25rem)]">
+    <div className="w-screen grow flex justify-center place-items-center">
       <NavTitle />
     </div>
   );
