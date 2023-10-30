c27f2284b863f4ae7b3f39c8b2a67dfa56ce7daf
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-10-30 09:18:31 +0800

    feat(translatable-context): add cursor handler on hovering

--- components/translatable-context.tsx
+++ components/translatable-context.tsx
@@ -12,14 +12,14 @@ const TranslatableContext = ({ className = "", children }: Props) => {
   const [translateX, setTranslateX] = useState(0);
   const [translateY, setTranslateY] = useState(0);
 
-  const elementRef = useRef(null);
+  const elementRef = useRef<HTMLDivElement>(null);
   const { elX, elY } = useMouse(elementRef);
   const [ref, { width, height }] = useMeasure<HTMLDivElement>();
 
   useEffect(() => {
     if (elX >= 0 && elY >= 0 && elX <= width && elY <= height) {
       setTranslateX(((elX - width / 2) * 10) / width);
-      setTranslateY(((elY - height / 2) * 10) / height);
+      setTranslateY(((elY - height / 2) * 20) / height);
     } else {
       setTranslateX(0);
       setTranslateY(0);
@@ -27,34 +27,50 @@ const TranslatableContext = ({ className = "", children }: Props) => {
   }, [elX, elY, width, height]);
 
   return (
-    <div
-      onMouseEnter={() => {
-        document.getElementById("cursor")?.classList.add("opacity-0");
-      }}
-      onMouseLeave={() => {
-        document.getElementById("cursor")?.classList.remove("opacity-0");
-      }}
-      className="flex flex-col"
-    >
-      <div ref={elementRef} className="w-max h-max">
-        <div ref={ref}>
-          <div
-            className={`
+    <div ref={ref}>
+      <div
+        onMouseEnter={() => {
+          const cursorElement = document.getElementById("cursor")!;
+          cursorElement.classList.add(
+            "opacity-50",
+            "transition-[top,left]",
+            "lock"
+          );
+          cursorElement.style.width = width + 8 + "px";
+          cursorElement.style.height = height + 8 + "px";
+        }}
+        onMouseMove={() => {
+          const cursorElement = document.getElementById("cursor")!;
+          cursorElement.style.left = elementRef.current!.offsetLeft + width / 2 + "px";
+          cursorElement.style.top = elementRef.current!.offsetTop + height / 2 + "px";
+        }}
+        onMouseLeave={() => {
+          const cursorElement = document.getElementById("cursor")!;
+          cursorElement.classList.remove(
+            "opacity-50",
+            "transition-[top,left]",
+            "lock"
+          );
+          cursorElement.style.width = "40px";
+          cursorElement.style.height = "40px";
+        }}
+        ref={elementRef}
+        className="w-max h-max flex flex-col p-2"
+      >
+        <div
+          className={`
               ${className}
               w-max h-max
               inline-flex 
               items-center justify-center 
               rounded-md
               bg-transparent 
-              hover:bg-gray/50 
-              hover:cursor-none
             `}
+        >
+          <div
+            style={{ transform: `translate(${translateX}%, ${translateY}%)` }}
           >
-            <div
-              style={{ transform: `translate(${translateX}%, ${translateY}%)` }}
-            >
-              {children}
-            </div>
+            {children}
           </div>
         </div>
       </div>
