ce25692ffb2b51388d1a25e2394b87d38345bdab
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-11-04 17:42:09 +0800

    feat(index): add tech stack intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
 import NavTitle from "@/components/nav-title";
+import Cone from "public/cone.png"
 
 export default function Home() {
   return (
-    <div className="h-full flex justify-center place-items-center">
-      <NavTitle />
+    <div>
+      <div className="mt-[50vh] mb-[30vh]">
+        <NavTitle />
+      </div>
+      <div className="relative">
+        <div className="relative w-max m-auto">
+          <div className="text-3xl font-semibold">I Develop</div>
+          <div className="pl-20 text-9xl font-bold">
+            <div>Frontend</div>
+            <div>Backend</div>
+            <div>Database</div>
+            <div>Mobile App</div>
+            <div>NLP, LLM</div>
+          </div>
+        </div>
+        <Image src={Cone} alt="" width={300} height={300} className="" />
+      </div>
     </div>
   );
 }
