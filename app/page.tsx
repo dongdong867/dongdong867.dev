f40cd10392f669e5cb39868f8730a1695ba4abf1
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-11-08 23:56:00 +0800

    feat(index): implement tech stack to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import NavTitle from "@/components/nav-title";
-import Cone from "public/cone.png"
+import TechStack from "./components/tech-stack";
 
 export default function Home() {
   return (
@@ -8,19 +7,7 @@ export default function Home() {
       <div className="mt-[50vh] mb-[30vh]">
         <NavTitle />
       </div>
-      <div className="relative">
-        <div className="relative w-max m-auto">
-          <div className="text-3xl font-semibold">I Develop</div>
-          <div className="pl-20 text-9xl font-bold">
-            <div>Frontend</div>
-            <div>Backend</div>
-            <div>Database</div>
-            <div>Mobile App</div>
-            <div>NLP, LLM</div>
-          </div>
-        </div>
-        <Image src={Cone} alt="" width={300} height={300} className="" />
-      </div>
+      <TechStack />
     </div>
   );
 }
