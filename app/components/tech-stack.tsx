318f5100dc7ae5aa3b21c7aafaf4c0456e5b1240
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-11-09 12:06:07 +0800

    feat(tech-stack): add rwd designs to component

--- app/components/tech-stack.tsx
+++ app/components/tech-stack.tsx
@@ -7,7 +7,7 @@ const techStack = () => {
     <div className="relative m-auto w-max h-max mb-20">
       <div className="relative w-max m-auto">
         <div className="mb-4 text-3xl font-semibold">I Develop</div>
-        <div className="pl-20 text-9xl font-bold tracking-tight">
+        <div className="pl-10 md:pl-20 text-5xl md:text-8xl lg:text-9xl font-bold md:tracking-tight">
           <div className="relative z-20">Frontend</div>
           <div>Backend</div>
           <div>Database</div>
@@ -15,8 +15,8 @@ const techStack = () => {
           <div className="relative z-20">NLP, LLM</div>
         </div>
       </div>
-      <Image src={Donut} alt="" width={300} height={300} className="absolute w-52 top-16 left-[70%] z-10" />
-      <Image src={Cone} alt="" width={300} height={300} className="absolute w-60 -left-20 -bottom-16" />
+      <Image src={Donut} alt="" width={300} height={300} className="absolute w-1/3 top-[12%] md:top-[5%] lg:top-[3%] left-[70%] z-10" />
+      <Image src={Cone} alt="" width={300} height={300} className="absolute w-1/3 -left-[8%] lg:-left-[10%] -bottom-[12%]" />
     </div>
   );
 };
