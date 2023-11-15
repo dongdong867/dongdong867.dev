9199b86cb0783bccf4e68b7b5873f63ae6af45ca
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-11-16 01:50:01 +0800

    feat(skill-stack): create skill stack data

---
+++ app/lib/skill-set-data.ts
@@ -0,0 +1,52 @@
+type SkillSetDataType = {
+  icon: string,
+  name: string
+}[]
+
+export const FirstLine: SkillSetDataType = [
+  { icon: "public/skill-stack/nextjs.png", name: "Next.js" },
+  { icon: "public/skill-stack/nuxt3.png", name: "Nuxt 3" },
+  { icon: "public/skill-stack/react.png", name: "React.js" },
+  { icon: "public/skill-stack/vue3js.png", name: "Vue3.js" },
+  { icon: "public/skill-stack/tailwindcss.png", name: "TailwindCSS" },
+  { icon: "public/skill-stack/framer-motion.png", name: "Framer Motion" },
+  { icon: "public/skill-stack/html.png", name: "HTML" },
+  { icon: "public/skill-stack/css.png", name: "CSS" },
+  { icon: "public/skill-stack/vercel.png", name: "Vercel" },
+]
+
+export const SecondLine: SkillSetDataType = [
+  { icon: "public/skill-stack/nestjs.png", name: "NestJS" },
+  { icon: "public/skill-stack/express.png", name: "Express.js" },
+  { icon: "public/skill-stack/spring.png", name: "Spring Boot" },
+  { icon: "public/skill-stack/java.png", name: "Java" },
+  { icon: "public/skill-stack/typescript.png", name: "TypeScript" },
+  { icon: "public/skill-stack/javascript.png", name: "JavaScript" },
+  { icon: "public/skill-stack/rust.png", name: "Rust" },
+  { icon: "public/skill-stack/nodejs.png", name: "NodeJS" },
+  { icon: "public/skill-stack/csharp.png", name: "C#" },
+]
+
+export const ThirdLine: SkillSetDataType = [
+  { icon: "public/skill-stack/graphql.png", name: "GraphQL" },
+  { icon: "public/skill-stack/mysql.png", name: "MySQL" },
+  { icon: "public/skill-stack/postgresql.png", name: "PostgreSQL" },
+  { icon: "public/skill-stack/mongodb.png", name: "MongoDB" },
+  { icon: "public/skill-stack/firebase.png", name: "Firebase" },
+  { icon: "public/skill-stack/prisma.png", name: "Prisma" },
+  { icon: "public/skill-stack/pwa.png", name: "PWA" },
+  { icon: "public/skill-stack/cryptojs.png", name: "CryptoJS" },
+  { icon: "public/skill-stack/gcp.png", name: "GCP" },
+  { icon: "public/skill-stack/aws.png", name: "AWS" },
+]
+
+export const ForthLine: SkillSetDataType = [
+  { icon: "public/skill-stack/python.png", name: "Python" },
+  { icon: "public/skill-stack/pytorch.png", name: "Pytorch" },
+  { icon: "public/skill-stack/transformers.png", name: "Transformers" },
+  { icon: "public/skill-stack/selenium.png", name: "Selenium" },
+  { icon: "public/skill-stack/flutter.png", name: "Flutter" },
+  { icon: "public/skill-stack/swift.png", name: "Swift" },
+  { icon: "public/skill-stack/swiftui.png", name: "SwiftUI" },
+  { icon: "public/skill-stack/uikit.png", name: "UiKit" },
+]
