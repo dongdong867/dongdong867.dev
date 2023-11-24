8346edf21a304310c458aa6929aa96012322fb8d
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-11-24 23:25:48 +0800

    feat(index): add skill set to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import NavTitle from "@/components/nav-title";
 import TechStack from "./components/tech-stack";
+import SkillSet from "./components/skill-set";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-y-20">
       <div className="mt-[50vh] mb-[30vh]">
         <NavTitle />
       </div>
       <TechStack />
+      <SkillSet />
     </div>
   );
 }
