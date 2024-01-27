62be15af834aa544be1a8c7c1c87ff1b84fe8689
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-27 10:01:55 +0800

    feat(navbar): add link to home page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import dong_logo from "@/public/dong-logo.svg";
 import NavigationMenu from "./navigation-menu";
+import Link from "next/link";
 
 const Navbar = () => (
   <div className="h-max flex-grow-0 flex justify-between place-items-center mx-10 mt-4 max-sm:mx-6">
-    <Image
-      src={dong_logo}
-      alt="dong-logo"
-      width={200}
-      height={50}
-      className="w-max h-5 ring-0 dark:invert"
-    />
+    <Link href={"/"}>
+      <Image
+        src={dong_logo}
+        alt="dong-logo"
+        width={200}
+        height={50}
+        className="w-max h-5 ring-0 dark:invert"
+      />
+    </Link>
     <div className="flex place-items-center">
       <NavigationMenu />
     </div>
