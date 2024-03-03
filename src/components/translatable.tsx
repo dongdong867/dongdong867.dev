9816634a7c5e96587353082d336e7e66d60a5150
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-03 18:34:05 +0800

    feat(translatable): create component for translatable context

---
+++ src/components/translatable.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useEffect, useRef, useState } from "react";
+import { useMeasure, useMouse } from "react-use";
+
+type Props = {
+  className?: string;
+  children: React.ReactNode;
+};
+export const Translatable = ({ className = "", children }: Props) => {
+  const [translateX, setTranslateX] = useState(0);
+  const [translateY, setTranslateY] = useState(0);
+
+  const elementRef = useRef<HTMLDivElement>(null);
+  const { elX, elY } = useMouse(elementRef);
+  const [ref, { width, height }] = useMeasure<HTMLDivElement>();
+
+  useEffect(() => {
+    if (elX >= 0 && elY >= 0 && elX <= width && elY <= height) {
+      setTranslateX(((elX - width / 2) * 10) / width);
+      setTranslateY(((elY - height / 2) * 20) / height);
+    } else {
+      setTranslateX(0);
+      setTranslateY(0);
+    }
+  }, [elX, elY, width, height]);
+
+  return (
+    <div ref={ref}>
+      <div ref={elementRef} className={cn("w-max h-max", className)}>
+        <div style={{ transform: `translate(${translateX}%, ${translateY}%)` }}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
