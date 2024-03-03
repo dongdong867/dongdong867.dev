6b1ee5b79c823933f221b258172c903c67b8e78c
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-03 16:46:25 +0800

    chore: add tailwind, prettier settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -18,6 +18,31 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        primary: "rgb(var(--primary))",
+        foreground: "rgb(var(--foreground))",
+        background: "rgb(var(--background))",
+        white: "rgb(var(--white))",
+        black: "rgb(var(--black))",
+        red: "rgb(var(--red))",
+        orange: "rgb(var(--orange))",
+        yellow: "rgb(var(--yellow))",
+        green: "rgb(var(--green))",
+        mint: "rgb(var(--mint))",
+        teal: "rgb(var(--teal))",
+        cyan: "rgb(var(--cyan))",
+        blue: "rgb(var(--blue))",
+        indigo: "rgb(var(--indigo))",
+        purple: "rgb(var(--purple))",
+        pink: "rgb(var(--pink))",
+        brown: "rgb(var(--brown))",
+        gray: "rgb(var(--gray))",
+        gray2: "rgb(var(--gray2))",
+        gray3: "rgb(var(--gray3))",
+        gray4: "rgb(var(--gray4))",
+        gray5: "rgb(var(--gray5))",
+        gray6: "rgb(var(--gray6))",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -34,7 +59,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require("tailwindcss-animate"), "prettier-plugin-tailwindcss"],
+} satisfies Config;
 
-export default config
+export default config;
