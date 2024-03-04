8ea6baad88fdfa994b631c14a713236714b9bb94
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-04 19:34:50 +0800

    feat(data): add apps with icon and label to data folder

---
+++ src/app/@mac/data/apps.ts
@@ -0,0 +1,10 @@
+export const apps = [
+  { icon: "finder.png", label: "Finder" },
+  { icon: "mail.png", label: "Mail" },
+  { icon: "linkedin.png", label: "LinkedIn" },
+  { icon: "github.png", label: "GitHub" },
+  { icon: "discord.png", label: "Discord" },
+  { icon: "terminal.png", label: "Terminal" },
+  { icon: "notes.png", label: "Notes" },
+  { icon: "docs.png", label: "Resume" },
+];
