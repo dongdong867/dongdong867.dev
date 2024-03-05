7997d4b6ebfe7fdb32f19996470251ec4cb264c3
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-05 13:18:26 +0800

    feat(finder): create application tab

---
+++ src/app/@mac/components/finder/application.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import { apps } from "../../data/apps";
+import Image from "next/image";
+
+export const Application = () => {
+  return (
+    <div className={cn("w-max px-6 py-4")}>
+      <div className={cn("grid grid-cols-4 gap-x-8 gap-y-8 place-items-center")}>
+        {apps.map((app) => {
+          return (
+            <div key={app.label} className={cn("col-span-1 space-y-1")}>
+              <Image
+                src={`/mac/apps/${app.icon}`}
+                alt=""
+                width={60}
+                height={60}
+                className={cn("aspect-square p-0.5 rounded-md")}
+              />
+              <div className={cn("text-sm text-center")}>{app.label}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
