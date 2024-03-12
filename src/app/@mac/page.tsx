2728857a92fcbfa03b99277ab72ca84dcef00b46
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-12 20:59:15 +0800

    feat(mac): create mac page

--- src/app/@mac/page.tsx
+++ src/app/@mac/page.tsx
@@ -1,10 +1,17 @@
 import { cn } from "@/lib/utils";
 import { Finder } from "./components/finder/finder";
+import { Terminal } from "./components/terminal/terminal";
 
 const MacHome = () => {
   return (
-    <div className={cn("h-screen bg-mac-light dark:bg-mac-dark bg-cover bg-center bg-origin-content bg-no-repeat")}>
+    <div
+      className={cn(
+        "w-full h-screen bg-mac-light dark:bg-mac-dark",
+        "bg-cover bg-center bg-origin-content bg-no-repeat"
+      )}
+    >
       <Finder />
+      <Terminal />
     </div>
   );
 };
