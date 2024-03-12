e5b0cfef50c16a38017fc4f4e6fbcd175fb1e229
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-12 20:58:38 +0800

    feat(window): add z-index settings to window

--- src/app/@mac/components/window.tsx
+++ src/app/@mac/components/window.tsx
@@ -5,11 +5,30 @@ import { motion } from "framer-motion";
 import { useLayoutEffect, useRef, useState } from "react";
 import { useWindowSize } from "react-use";
 
+const setZIndex = (id: string) => {
+  const idList = ["finder", "terminal"];
+  idList.forEach((app) => {
+    if (app == id) {
+      const window = document.getElementById(app);
+      if (window) {
+        window.classList.add("z-10");
+      }
+    } else {
+      const window = document.getElementById(app);
+      if (window) {
+        window.classList.remove("z-10");
+      }
+    }
+  });
+};
+
 export const ApplicationWindow = ({
+  id,
   detectUpdated,
   className,
   children,
 }: {
+  id: string;
   detectUpdated?: string;
   className?: string;
   children: React.ReactNode;
@@ -25,6 +44,7 @@ export const ApplicationWindow = ({
 
   return (
     <motion.div
+      id={id}
       drag
       dragMomentum={false}
       dragConstraints={{
@@ -34,8 +54,17 @@ export const ApplicationWindow = ({
         bottom: windowHeight - 20,
       }}
       dragElastic={0}
+      onClick={() => {
+        setZIndex(id);
+      }}
+      onDragEnd={(_, info) => {
+        if (ref.current) {
+          ref.current.style.top = info.delta.x + "px";
+          ref.current.style.height = info.delta.y + "px";
+        }
+      }}
       ref={ref}
-      className={cn(className)}
+      className={cn("absolute", className)}
     >
       {children}
     </motion.div>
