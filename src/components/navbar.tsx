08feb4032db888a916acbacdcc3541532027697f
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-13 09:26:43 +0800

    feat(navbar): create navbar with links

---
+++ src/components/navbar.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import ThemeToggle from "./theme/theme-toggle";
+import Link from "next/link";
+import { Translatable } from "./translatable";
+
+export const Navbar = () => {
+  return (
+    <div
+      className={cn(
+        "fixed w-full max-w-md max-md:w-11/12 left-1/2 -translate-x-1/2",
+        "mt-4 px-4 bg-gray5 rounded-full z-10",
+        "flex justify-between place-items-center"
+      )}
+    >
+      <Link href={"/"}>
+        <Translatable className={cn("p-1.5")}>
+          <Image
+            src={"/dong-icon_320px.png"}
+            alt=""
+            width={40}
+            height={40}
+            className={cn("size-10 aspect-square p-1.5 invert dark:invert-0")}
+          />
+        </Translatable>
+      </Link>
+
+      <div className={cn("flex font-medium")}>
+        <Translatable className={cn("p-1.5")}>
+          <Link href={"/works"}>Works</Link>
+        </Translatable>
+        <Translatable className={cn("p-1.5")}>
+          <Link href={"/about"}>About</Link>
+        </Translatable>
+        <Translatable className={cn("p-1.5")}>
+          <Link href={"/contact"}>Contact</Link>
+        </Translatable>
+      </div>
+
+      <ThemeToggle />
+    </div>
+  );
+};
