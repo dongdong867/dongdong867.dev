1483ee4c0239e58a8df94025e2dcbc11183e2b2f
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-13 11:31:47 +0800

    feat(work-stack): change work stack component

--- src/app/components/work-stack.tsx
+++ src/app/components/work-stack.tsx
@@ -1,13 +1,44 @@
-import { InviewTransition } from "@/components/inview-transition";
+import Image from "next/image";
+import Donut from "/public/home/donut.png";
+import Cone from "/public/home/cone.png";
 import { cn } from "@/lib/utils";
 
 export const WorkStack = () => {
   return (
-    <InviewTransition className={cn("py-2 -space-y-px")}>
-      <div>A Fullstack Developer</div>
-      <div>A Mobile Application Developer</div>
-      <div>A NLP Engineer</div>
-      <div>A Tech Lover</div>
-    </InviewTransition>
+    <div
+      style={{ colorScheme: "dark" }}
+      className={cn(
+        "w-screen h-[50dvh] md:h-[120dvh] flex m-auto text-white bg-black py-10"
+      )}
+    >
+      <div className="relative m-auto w-max h-max">
+        <div className="relative w-max m-auto">
+          <div className="mb-4 text-xl md:text-3xl font-semibold">
+            I Develop
+          </div>
+          <div className="pl-10 md:pl-20 text-4xl md:text-7xl xl:text-8xl font-bold md:tracking-tight">
+            <div className="relative z-20">Frontend</div>
+            <div>Backend</div>
+            <div>Database</div>
+            <div>Mobile App</div>
+            <div className="relative z-20">NLP, LLM</div>
+          </div>
+        </div>
+        <Image
+          src={Donut}
+          alt=""
+          width={300}
+          height={300}
+          className="absolute w-1/3 top-[12%] md:top-[5%] lg:top-[3%] left-[70%] z-10"
+        />
+        <Image
+          src={Cone}
+          alt=""
+          width={300}
+          height={300}
+          className="absolute w-1/3 -left-[8%] lg:-left-[10%] -bottom-[12%]"
+        />
+      </div>
+    </div>
   );
 };
