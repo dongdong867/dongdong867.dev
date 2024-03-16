4d9f6debbb6d9bd45a9b17a35332040599fe4715
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-16 16:08:23 +0800

    feat(works): fix font size overflow in oncoming block

--- src/app/works/components/oncoming.tsx
+++ src/app/works/components/oncoming.tsx
@@ -7,12 +7,12 @@ export const Oncoming = () => {
     <Card>
       <div
         className={cn(
-          "w-max h-max m-auto text-gray text-5xl font-medium tracking-tight"
-        )}
+          "w-max h-max m-auto text-gray text-2xl md:text-5xl font-medium tracking-tight"
+          )}
       >
         <Typewriter
           words={[{ text: "More works oncoming..." }]}
-          cursorClassName={cn("h-12 w-1")}
+          cursorClassName={cn("h-8 md:h-12 w-0.5 md:w-1")}
         />
       </div>
     </Card>
